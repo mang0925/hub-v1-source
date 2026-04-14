6c5ce7c1bac8fdd7c6e68d9be9a633a65beebec2
mang0925 <jamoisnael8@gmail.com>
2026-04-14 10:55:34 +0200

    Update hub v1.lua

--- hub v1.lua
+++ hub v1.lua
@@ -11,7 +11,7 @@ local player = Players.LocalPlayer
 local lp = player  -- Alias for compatibility
 
 local Whitelist = {
-    "tropmignon44",
+    "sasuke409867",
     "MANGGO_497",
     "99night_072",
     "sogane1234",
